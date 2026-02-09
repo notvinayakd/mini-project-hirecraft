488b4accac4150a5d6d2c39972e5b0fd4e314fce
Athmaja04 <athmajaajith@gmail.com>
2026-02-09 11:28:53 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Athmaja@230560'
-    MYSQL_DB = 'placement_portal'
-    JWT_SECRET_KEY = 'super-secret-key'
--- backend/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_mysqldb import MySQL
-from flask_jwt_extended import JWTManager
-
-mysql = MySQL()
-jwt = JWTManager()
